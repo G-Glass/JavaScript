70ad25391784efaa4b344b904a50582517f31525
Sataca <glassg767@gmail.com>
2025-08-27 21:09:55 +0200

    commit

--- javascript/script11.js
+++ javascript/script11.js
@@ -3,3 +3,32 @@ var a = 45
 if (a == 43) {
     console.log("parabens")
 }
+var divo = document.querySelector("img#i1")
+divo.addEventListener("mouseout", saida)
+function academy() {
+   //divo.innerHTMl = "Clicou no menu"
+   // divo.style.border= "2px solid red"
+   var p = document.querySelector("p.para")
+   p.style.display="block"
+   /*if (p.style.display="block") {
+    function academy() {
+        var p = document.querySelector("p.para")
+        p.style.display="none"
+    }
+   }*/
+  divo.style.display="none"
+  var i = document.querySelector("img#im1")
+  i.style.display="block"
+}
+function sai() {
+    var i = document.querySelector("img#im1")
+    var p = document.querySelector("p.para")
+    p.style.display="none"
+    i.style.display="none"
+    var divo = document.querySelector("img#i1")
+    divo.style.display="block"
+}
+function saida() {
+    //divo.style.border="2px solid black"
+   //divo.innerHTML="Academia"
+}
