4989c2c4a845d06165573adfb2ae101244150d27
Sataca <glassg767@gmail.com>
2025-09-21 08:13:34 +0200

    commit

---
+++ javascript/script25.js
@@ -0,0 +1,2 @@
+let num = [ 4, 3, 5, 7] //FORMADO DE ARRAY/VETOR
+console.log(`O nosso vetor é ${num}  `)
--- javascript/tab.js
+++ javascript/tab.js
@@ -1,34 +1,26 @@
 //alert("Olá")
 
-const { createElement } = require("react")
-
 function cal() {
     let n1 = document.querySelector("#valor")
     let n2 = document.querySelector("#valor2")
     let resto = document.querySelector("#tudo")
     let multi = document.querySelector("#somas")
-    let vezes = document.querySelector("#×")
+    let res = document.querySelector(".tudo")
     let v1 = Number(n1.value)
     let v2 = Number(n2.value)
 
+    resto.innerHTML ="" // LIMPA AREA OU DADOS
     for (let q = 0 ; q <= v2 ; q++) {
         let item = document.createElement('option')
         item.text = ` ${v1} + ${q} = ${v1+q}`
+        res.innerHTML = `${v1} + ${q} = ${v1+q}`
         resto.appendChild(item)
         item.value = `procure${q}`
 
 
+
     }
-    tab.innerHTML ="" // LIMPA AREA OU DADOS
-        for (let q = 0 ; q <= v2 ; q++) {
-        let item = document.createElement('option')
-        item.text = ` ${v1} × ${q} = ${v1*q}`
-        resto.appendChild(item)
-        item.value = `procure${q}`
-      
-    }
-    
-    
+     
     
     
     setTimeout(exibirProximo, 200); // ATUALIZA O NAVEGADOR A CADA 200 MILISEGUNDOS
