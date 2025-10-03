8071c4a6a3f92917360252a8c763d987fd10d300
Sataca <glassg767@gmail.com>
2025-10-03 20:35:21 +0200

    commit

---
+++ javascript/array_functions/index.js
@@ -0,0 +1,13 @@
+let valor = document.querySelector("#valor1")
+let resposta = document.querySelector("#recebe")
+
+let v1 = Number(valor.value)
+let r = resposta
+
+function analise() {
+    return  v1*23
+    
+
+    
+}
+r.innerHTML = ` Os valores são dois ${}`
