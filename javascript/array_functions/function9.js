f37a722987ceade82cfe969087935c9d7679b2b1
Sataca <glassg767@gmail.com>
2025-10-17 21:17:27 +0200

    commit

---
+++ javascript/array_functions/function9.js
@@ -0,0 +1,12 @@
+import { numeros } from "./modulos.js";
+
+let i = 3
+let s = 8
+let a =  function num(e,r) {
+
+    const nu = numeros.reduce((adiciona,numer) => adiciona+numer,0)
+    return nu+(e+r)
+    
+}
+let x = a(i,s)
+console.log(`O resultado é ${x}`)
